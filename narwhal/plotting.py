c8a54f6d4c3f8e059cba8c297aea0cbfcc3dbce1
Nat Wilson <njwilson23@gmail.com>
2014-01-23 10:49:21 -0500

    improve ccmean* functions

--- narwhal/plotting.py
+++ narwhal/plotting.py
@@ -1,5 +1,6 @@
 import itertools
 import operator
+import copy
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import griddata
@@ -7,18 +8,39 @@ import gsw
 from karta import Point, Line, LONLAT
 from narwhal.cast import Cast, CastCollection
 
-def ccmean(cc):
-    if False in (np.all(cc[0]["pres"] == c["pres"]) for c in cc[1:]):
+def ccmeans(cc):
+    """ Calculate a mean Cast along isopycnals from a CastCollection. """
+    c0 = cc[0]
+    #c0 = cc[np.argmax(
+    s0 = c0["sigma"]
+    
+    sharedkeys = set(c0.data.keys()).intersection(
+                    *[set(c.data.keys()) for c in cc[1:]]).difference(
+                    set(("pres", "botdepth", "time")))
+
+    data = dict()
+    for key in sharedkeys:
+        arr = np.empty((len(c0["pres"]), len(cc)))
+        arr[:,0] = c0[key]
+        for j, c in enumerate(cc[1:]):
+            s = np.convolve(c["sigma"], np.ones(3)/3.0, mode="same")
+            arr[:,j+1] = np.interp(s0, s, c[key])
+        data[key] = np.nanmean(arr, axis=1)
+    
+    return Cast(copy.copy(c0["pres"]), **data)
+
+def ccmeanp(cc):
+    if False in (all(cc[0]["pres"] == c["pres"]) for c in cc[1:]):
         raise ValueError("casts must share pressure levels")
     p = cc[0]["pres"]
     data = dict()
     # shared keys are those in all casts, minus pressure and botdepth
     sharedkeys = set(cc[0].data.keys()).intersection(
                     *[set(c.data.keys()) for c in cc[1:]]).difference(
-                    set(("pres", "botdepth")))
+                    set(("pres", "botdepth", "time")))
     for key in sharedkeys:
-        valarray = np.vstack([c.data[key] for c in cc])
-        data[key] = np.nanmean(valarray, axis=0)
+        arr = np.vstack([c.data[key] for c in cc])
+        data[key] = np.nanmean(arr, axis=0)
     return Cast(p, **data)
 
 ###### T-S plots #######
@@ -37,7 +59,7 @@ def plot_ts(*casts, **kwargs):
     if average_collections:
         for i, cast in enumerate(casts):
             if isinstance(cast, CastCollection):
-                cast = ccmean(cast)
+                cast = ccmeanp(cast)
             plt.plot(cast["sal"], cast["theta"], styles.next(), label=labels[i],
                  **kwargs)
 
