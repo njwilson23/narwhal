579cdb8245ad2f6d5f79e170b8189a93db354389
Nat Wilson <njwilson23@gmail.com>
2014-01-24 16:14:24 -0500

    use util.ccmeanp

--- narwhal/plotting.py
+++ narwhal/plotting.py
@@ -1,47 +1,15 @@
 import itertools
 import operator
-import copy
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import griddata
 import gsw
 from karta import Point, Line, LONLAT
 from narwhal.cast import Cast, CastCollection
+import narwhal.util as util
 
-def ccmeans(cc):
-    """ Calculate a mean Cast along isopycnals from a CastCollection. """
-    c0 = cc[0]
-    s0 = c0["sigma"]
-    sharedkeys = set(c0.data.keys()).intersection(
-                    *[set(c.data.keys()) for c in cc[1:]]).difference(
-                    set(("pres", "botdepth", "time")))
-
-    data = dict()
-    for key in sharedkeys:
-        arr = np.empty((len(c0["pres"]), len(cc)))
-        arr[:,0] = c0[key]
-        for j, c in enumerate(cc[1:]):
-            s = np.convolve(c["sigma"], np.ones(3)/3.0, mode="same")
-            arr[:,j+1] = np.interp(s0, s, c[key])
-        data[key] = np.nanmean(arr, axis=1)
-    
-    return Cast(copy.copy(c0["pres"]), **data)
-
-def ccmeanp(cc):
-    print cc
-    print len(cc)
-    if False in (np.all(cc[0]["pres"] == c["pres"]) for c in cc):
-        raise ValueError("casts must share pressure levels")
-    p = cc[0]["pres"]
-    data = dict()
-    # shared keys are those in all casts, minus pressure and botdepth
-    sharedkeys = set(cc[0].data.keys()).intersection(
-                    *[set(c.data.keys()) for c in cc[1:]]).difference(
-                    set(("pres", "botdepth", "time")))
-    for key in sharedkeys:
-        arr = np.vstack([c.data[key] for c in cc])
-        data[key] = np.nanmean(arr, axis=0)
-    return Cast(p, **data)
+ccmeanp = util.ccmeanp
+ccmeans = util.ccmeans
 
 ###### T-S plots #######
 
