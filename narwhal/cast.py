njwilson23/narwhal

commit eb47a8f216bb7c66dd8ce96b7bf62f1212d0e43a

make primary key of XBTs depth, not pressure

narwhal/cast.py
@@ -425,9 +425,11 @@ class XBTCast(Cast):
     """ Specialization of Cast with temperature field. """
     _type = "xbtcast"
 
-    def __init__(self, p, temp, coords=None, properties=None, **kwargs):
+    def __init__(self, z, temp, coords=None, properties=None,
+                 primarykey="z", **kwargs):
         super(XBTCast, self).__init__(p, temp=temp, coords=coords,
-                                      properties=properties, **kwargs)
+                                      properties=properties, primarykey=primarykey,
+                                      **kwargs)
         return
 
 

commit 75d04cdc6118dc4b975dd77c1f62710360f87eff

fix mistake introduced with eb47a8f216bb7c66dd8ce96b7bf62f1212d0e43a

narwhal/cast.py
@@ -427,7 +427,7 @@ class XBTCast(Cast):
 
     def __init__(self, z, temp, coords=None, properties=None,
                  primarykey="z", **kwargs):
-        super(XBTCast, self).__init__(p, temp=temp, coords=coords,
+        super(XBTCast, self).__init__(z, temp=temp, coords=coords,
                                       properties=properties, primarykey=primarykey,
                                       **kwargs)
         return