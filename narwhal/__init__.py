ee66d11eae1af5bf71a2324ea92e94769102237e
Nat Wilson <njwilson23@gmail.com>
2014-10-14 12:30:20 -0400

    add plotting __init__.py

--- narwhal/__init__.py
+++ narwhal/__init__.py
@@ -5,7 +5,7 @@ from .cast import CastCollection, read
 from .bathymetry import Bathymetry
 from . import gsw
 from . import util
-#from . import plotting
+from . import plotting
 
 __all__ = ["cast", "bathymetry", "gsw", "util"]
 
---
+++ narwhal/plotting/__init__.py
@@ -0,0 +1,6 @@
+from . import section_plot
+from . import property_plot
+from .plotting import plot_profiles, plot_map
+from .property_plot import plot_ts
+from . import plotutil
+
