8f57aaf51e10ab39fc18824cabaded341e49c2f2
Nat Wilson <njwilson23@gmail.com>
2015-06-04 16:58:37 -0700

    import plot_section function

--- narwhal/plotting/__init__.py
+++ narwhal/plotting/__init__.py
@@ -1,6 +1,6 @@
 from . import section_plot
 from . import property_plot
-from .plotting import plot_profiles, plot_map
+from .plotting import plot_profiles, plot_map, plot_section
 from .property_plot import plot_ts
 from . import plotutil
 
