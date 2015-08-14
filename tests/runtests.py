f147ead811f129e168d7a43010875d2222268f77
Nat Wilson <njwilson23@gmail.com>
2015-08-14 10:30:45 -0200

    move analysis functions to seperate test file

--- tests/runtests.py
+++ tests/runtests.py
@@ -3,11 +3,12 @@ import matplotlib
 matplotlib.use("Agg")
 
 from cast_tests import *
+from analysis_tests import *
+from fraction_tests import *
 from bathymetry_tests import *
 from io_tests import *
 from misc_tests import *
 from plot_tests import *
-from fraction_tests import *
 
 if __name__ == "__main__":
     unittest.main()
