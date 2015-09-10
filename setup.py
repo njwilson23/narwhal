c8f573f9227291a9905b6c5f6ea94596ae14c24a
Nat Wilson <njwilson23@gmail.com>
2015-09-10 16:17:20 -0400

    WIP: upgrade to GSW 3.05

--- setup.py
+++ setup.py
@@ -1,40 +1,15 @@
-from __future__ import print_function
-import os, shutil
 from distutils.core import setup
 from distutils.extension import Extension
-import install_gsw
 
-try:
-    print("Fetching GSW v3.03", end="")
-    install_gsw.download_zip("http://www.teos-10.org/software/gsw_c_v3.03.zip",
-                             fnm="temp_gsw_c.zip")
-
-    if install_gsw.compare_md5("temp_gsw_c.zip", "1317c63c36bb4ee4f438c573d5bea2db"):
-        install_gsw.unzip("temp_gsw_c.zip", "deps/")
-        ext = [Extension("narwhal.cgsw",
-                         sources=["deps/gsw_c_v3.03/gsw_oceanographic_toolbox.c",
-                                  "deps/gsw_c_v3.03/gsw_saar.c"], 
-                         include_dirs=["deps/gsw_c_v3.03/"])]
-
-    else:
-        raise Exception("MD5 for downloaded GSW source doesn't match "
-                        "expected digest. GSW will not be installed.")
-    print("...done")
-
-except Exception as e:
-    print("\nFailed to download and install Gibbs Seawater Toolbox")
-    print(e)
-    ext = []
-
-finally:
-    if os.path.isfile("temp_gsw_c.zip"):
-        os.remove("temp_gsw_c.zip")
+ext_gsw = Extension("narwhal.cgsw",
+                    sources=["deps/gsw_v_v3.05/gsw_oceanographic_toolbox.c",
+                             "deps/gsw_c_v3.05/gsw_saar.c"],
+                    include_dirs=["deps/gsw_c_v3.03"])
 
 setup(
     name = "narwhal",
-    version = "0.4.0b",
+    version = "0.4.0",
     author = "Nat Wilson",
     packages = ["narwhal", "narwhal.plotting"],
-    ext_modules = ext,
+    ext_modules = ext_gsw,
 )
-
