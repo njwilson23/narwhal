0ccdb73cb38783f2e4cf5aacd4a66fe1068f5d26
Nat Wilson <njwilson23@gmail.com>
2015-09-12 13:21:18 -0400

    WIP implementation of inverse computation

--- narwhal/geo.py
+++ narwhal/geo.py
@@ -6,7 +6,7 @@ bare-bones geometrical and geodetic capabilities.
 """
 
 from __future__ import print_function, division
-from math import sqrt, sin, cos, tan, atan, atan2, pi
+from math import sqrt, sin, cos, acos, tan, atan, atan2, pi
 from scipy.optimize import brentq
 
 class CoordinateSystem(object):
@@ -30,8 +30,21 @@ class LonLat(CoordinateSystem):
         return x, y
 
     def forward(self, x, y, azimuth, distance):
-        """ Compute the final position after moving from (x, y) in the *azimuth*
-        direction for *distance* meters
+        """ Compute the destination reached starting from a point and travelling
+        in a specified direction.
+
+        Parameters
+        ----------
+        x:          longitude at start
+        y:          latitude at start
+        azimuth:    direction travelled from point
+        distnce:    distance travelled
+
+        Returns
+        -------
+        x2:         longitude at destination
+        y2:         latitude at destination
+        back_az:    back azimuth from destination
 
         Algorithm due to Karney, C.F.F. "Algorithms for geodesics", J. Geod (2013)
         """
@@ -44,14 +57,14 @@ class LonLat(CoordinateSystem):
         beta1 = atan((1-f)*tan(phi1))
         _i = sqrt(cos(alpha1)**2 + (sin(alpha1)*sin(beta1))**2)
         alpha0 = atan2(sin(alpha1)*cos(beta1), _i)
-        sigma1 = atan2(sin(beta1), cos(alpha1)*cos(beta1))
-        omega1 = atan2(sin(alpha0)*sin(sigma1), cos(sigma1))
+        # sigma1 = atan2(sin(beta1), cos(alpha1)*cos(beta1))
+        # omega1 = atan2(sin(alpha0)*sin(sigma1), cos(sigma1))
+        sigma1, omega1 = _solve_NEA(alpha0, alpha1, beta1)
 
         # Determine sigma2
-        eps2 = (f*(2-f))
-        eps = sqrt(eps2)
-        second_eps = sqrt(eps2 / (1-eps2))
-        k2 = (second_eps * cos(alpha0))**2
+        eccn2 = (f*(2-f))
+        second_eccn2 = eccn2 / (1-eccn2)
+        k2 = second_eccn2*cos(alpha0)**2
 
         _rad = sqrt(1+k2)
         eps = (_rad - 1) / (_rad + 1)
@@ -111,10 +124,27 @@ class LonLat(CoordinateSystem):
         if x2 >= 180.0:
             x2 -= 360.0
         y2 = phi2*180.0/pi
-        return x2, y2
+        backaz = (alpha2+pi)*180/pi
+        return x2, y2, backaz
 
     def inverse(self, x1, y1, x2, y2):
-        """ Compute the shortest path (geodesic) between two points """
+        """ Compute the shortest path (geodesic) between two points.
+
+        Parameters
+        ----------
+        x1:         first longitude
+        y1:         first latitude
+        x2:         second longitude
+        y2:         second latitude
+
+        Returns
+        -------
+        az:         forward azimuth from first point
+        back_az:    back azimuth from second point
+        distance:   distance between points
+
+        Algorithm due to Karney, C.F.F. "Algorithms for geodesics", J. Geod (2013)
+        """
 
         if y1 == y2 == 0.0:
             # Equatorial case
@@ -165,12 +195,134 @@ class LonLat(CoordinateSystem):
         assert y1 <= y2 <= -y1
         assert 0 <= x2-x1 <= 180.0
 
+        phi1 = y1*pi/180.0
+        phi2 = y2*pi/180.0
+        f = (self.a-self.b) / self.a
 
+        # Guess the azimuth
+        if (abs(lambda12-pi) > 0.0087) or (abs(phi1+phi2) > 0.0087):
+            # not nearly antipodal
+            alpha1, _, _ = solve_vicenty(self.a, f, lambda12, phi1, phi2)
+        else:
+            alpha1 = solve_astroid(self.a, f, lambda12, phi1, phi2)
 
+        # alpha1 = 161.914*pi/180     # DEBUG
+        alpha1 += 1
 
+        beta1 = atan((1-f)*tan(phi1))
+        beta2 = atan((1-f)*tan(phi2))
 
-        backaz = az + pi
-        return az, backaz, distance
+        # Newton iteration
+        niter = 0
+        maxiter = 20
+        tol = 1/self.a*2*pi
+        dlambda12 = tol + 1
+
+        while (abs(dlambda12) > tol) and (niter != maxiter):
+
+            # Solve triangles
+            _i = sqrt(cos(alpha1)**2 + (sin(alpha1)*sin(beta1))**2)
+            alpha0 = atan2(sin(alpha1)*cos(beta1), _i)
+            sigma1, omega1 = _solve_NEA(alpha0, alpha1, beta1)
+            alpha2, sigma2, omega2 = _solve_NEB(alpha0, alpha1, beta1, beta2)
+
+            # Determine lambda12
+            eccn2 = f*(2-f)
+            second_eccn2 = eccn2 / (1-eccn2)
+            k2 = second_eccn2 * cos(alpha0)**2
+            _rad = sqrt(1+k2)
+            eps = (_rad - 1) / (_rad + 1)
+
+            n = f/(2-f)
+            n2 = n*n
+            A3 = 1 + (1.0/2 - 1.0/2*n)*eps - (1.0/3 + 1.0/8*n - 3.0/8*n**2)*eps**2 \
+                - (1.0/16 + 3.0/16*n + 1.0/16*n**2)*eps**3 - (3.0/64 + 1.0/32*n)*eps**4 \
+                - 3.0/128*eps**5
+
+            C3 = [(1.0/4 - n/4)*eps + (1.0/8 - n2/8)*eps**2 + (3.0/64 + 3.0*n/64 - n2/64)*eps**3 \
+                    + (5.0/128 + n/64)*eps**4 + 3.0/128*eps**5,
+                  (1.0/16 - 3.0*n/32 + n2/32)*eps**2 + (3.0/64 - n/32 - 3*n2/64)*eps**3 \
+                    + (3.0/128 + n/128)*eps**4 + 5.0/256*eps**5,
+                  (5.0/192 - 3.0*n/64 + 5.0*n2/192)*eps**3 + (3.0/128 - 5.0*n/192)*eps**4 \
+                    + 7.0/512*eps**5,
+                  (7.0/512 - 7.0*n/256)*eps**4 + 7.0*eps**5/512,
+                  21.0*eps**5/2560]
+
+            I3s1 = A3 * (sigma1 + sum(c*sin(2*(i+1)*sigma1) for i,c in enumerate(C3)))
+            I3s2 = A3 * (sigma2 + sum(c*sin(2*(i+1)*sigma2) for i,c in enumerate(C3)))
+
+            lambda1 = omega1 - f*sin(alpha0)*I3s1
+            lambda2 = omega2 - f*sin(alpha0)*I3s2
+            lambda12_next = lambda2 - lambda1
+            dlambda12 = lambda12_next - lambda12
+
+            if abs(dlambda12) > tol:
+                # Refine alpha1
+                A1 = 1.0/(1-eps) * (1 + eps**2/4 + eps**4/64 + eps**6/256)
+                C1 = [-1.0/2*eps + 3.0/16*eps**3 - 1.0/32*eps**5,
+                      -1.0/16*eps**2 + 1.0/32*eps**4 - 9.0/2048*eps**6,
+                      -1.0/48*eps**3 + 3.0/256*eps**5,
+                      -5.0/512*eps**4 + 3.0/512*eps**6,
+                      -7.0/1280*eps**5,
+                      -7.0/2048*eps**6]
+
+                I1s1 = A1 * (sigma1 + sum(c*sin(2*(i+1)*sigma1) for i,c in enumerate(C1)))
+                I1s2 = A1 * (sigma2 + sum(c*sin(2*(i+1)*sigma2) for i,c in enumerate(C1)))
+
+                A2 = (1-eps) * (1 + 1.0/4*eps**2 + 9.0/64*eps**4 + 25.0/256*eps**6)
+                C2 = [1.0/2*eps + 1.0/16*eps**3 + 1.0/32*eps**5,
+                      3.0/16*eps**2 + 1.0/32*eps**4 + 35.0/2048*eps**6,
+                      5.0/48*eps**3 + 5.0/256*eps**5,
+                      35.0/512*eps**4 + 7.0/512*eps**6,
+                      63.0/1280*eps**5,
+                      77.0/2048*eps**6]
+
+                I2s1 = A2 * (sigma1 + sum(c*sin(2*(i+1)*sigma1) for i,c in enumerate(C2)))
+                I2s2 = A2 * (sigma2 + sum(c*sin(2*(i+1)*sigma2) for i,c in enumerate(C2)))
+
+                Js1 = I1s1 - I2s1
+                Js2 = I1s2 - I2s2
+
+                m12 = self.b * (sqrt(1 + k2*sin(sigma2)**2) * cos(sigma1)*sin(sigma2) \
+                              - sqrt(1 + k2*sin(sigma1)**2) * sin(sigma1)*cos(sigma2) \
+                              - cos(sigma1) * cos(sigma2) * (Js2-Js1))
+                dlambda12_dalpha1 = m12/(self.a * cos(alpha2)*cos(beta2))
+                dalpha1 = -dlambda12 / (dlambda12_dalpha1)
+
+                alpha1 += dalpha1
+            niter += 1
+
+        # Determine s12
+        A1 = 1.0/(1-eps) * (1 + eps**2/4 + eps**4/64 + eps**6/256)
+        C1 = [-1.0/2*eps + 3.0/16*eps**3 - 1.0/32*eps**5,
+              -1.0/16*eps**2 + 1.0/32*eps**4 - 9.0/2048*eps**6,
+              -1.0/48*eps**3 + 3.0/256*eps**5,
+              -5.0/512*eps**4 + 3.0/512*eps**6,
+              -7.0/1280*eps**5,
+              -7.0/2048*eps**6]
+
+        I1s1 = A1 * (sigma1 + sum(c*sin(2*(i+1)*sigma1) for i,c in enumerate(C1)))
+        I1s2 = A1 * (sigma2 + sum(c*sin(2*(i+1)*sigma2) for i,c in enumerate(C1)))
+
+        s1 = I1s1*self.b
+        s2 = I1s2*self.b
+        s12 = s2-s1
+
+        az = (alpha1 % (2*pi))*180/pi
+        backaz = (alpha2+pi)*180/pi
+        return az, backaz, s12
+
+def _solve_NEA(alpha0, alpha1, beta1):
+    sigma1 = atan2(sin(beta1), cos(alpha1)*cos(beta1))
+    omega1 = atan2(sin(alpha0)*sin(sigma1), cos(sigma1))
+    return sigma1, omega1
+
+def _solve_NEB(alpha0, alpha1, beta1, beta2):
+    # alpha2 = sin(alpha0) / cos(beta2)     # Less accurate?
+    alpha2 = acos(sqrt(cos(alpha1)**2*cos(beta1)**2 + (cos(beta2)**2 - cos(beta1)**2)) / cos(beta2))
+    sigma2 = atan2(sin(beta2), cos(alpha2)*cos(beta2))
+    omega2 = atan2(sin(alpha0)*sin(sigma2), cos(sigma2))
+    return alpha2, sigma2, omega2
 
 def solve_astroid(a, f, lambda12, phi1, phi2):
     """ Used to provide an initial guess to the inverse problem by solving the
@@ -188,7 +340,7 @@ def solve_astroid(a, f, lambda12, phi1, phi2):
     -------
     alpha1:     estimated forward azimuth at first point
 
-    see Karney (2011) for details
+    see Karney (2013) J. Geod. for details
     """
     beta1 = atan((1-f) * tan(phi1))
     beta2 = atan((1-f) * tan(phi2))
@@ -216,12 +368,11 @@ def solve_vicenty(a, f, lambda12, phi1, phi2):
     alpha2:     forward azimuth at second point
     s12:        distance between points
 
-    see Karney (2011) for details
+    see Karney (2013) J. Geod. for details
     """
     eccn2 = f*(2-f)
     beta1 = atan((1-f) * tan(phi1))
     beta2 = atan((1-f) * tan(phi2))
-    printd(beta1, beta2)
     w = sqrt(1 - eccn2 * (0.5 * (cos(beta1) + cos(beta2)))**2)
     omega12 = lambda12 / w
 
@@ -236,9 +387,6 @@ def solve_vicenty(a, f, lambda12, phi1, phi2):
     alpha2 = atan2(z2_i, z2_r)
     s12 = a*w*sigma12
     return alpha1, alpha2, s12
-
-def _flattening(a, b):
-    return (a-b)/a
 
 def _degrees(r):
     return r*180/pi
@@ -278,11 +426,11 @@ class Line(MultipointBase):
 if __name__ == "__main__":
     # Karney's Table 2 example for the forward problem
     # print("Forward problem")
-    # x1, y1 = LonLatWGS84.forward(0.0, 40.0, 30.0, 10e6)
-    # print("Solution:", 137.844, 41.793)
-    # print(x1, y1)
+    # x1, y1, baz = LonLatWGS84.forward(0.0, 40.0, 30.0, 10e6)
+    # print("solution:", 137.84490004377, 41.79331020506, 149.09016931807)
+    # print("computed:", x1, y1, baz-180)
 
-    # vicenty problem
+    # vicenty problem, Table 3
     # print("\nVicenty")
     # a = 6378137.0
     # f = 1/298.257223563
@@ -293,13 +441,22 @@ if __name__ == "__main__":
     # print("solution", 77.043533, 77.043508, 4.944208)
     # print("computed", alpha1*180/pi, alpha2*180/pi, distance)
 
-    # astroid problem
-    print("\nAstroid")
-    a = 6378137.0
-    f = 1/298.257223563
-    phi1 = -30*pi/180
-    phi2 = 29.9*pi/180
-    lambda12 = 179.8*pi/180
-    alpha1 = solve_astroid(a, f, lambda12, phi1, phi2)
-    print("solution:", 161.914)
-    print("computed:", alpha1*180/pi)
+    # astroid problem, Table 4
+    # print("\nAstroid")
+    # a = 6378137.0
+    # f = 1/298.257223563
+    # phi1 = -30*pi/180
+    # phi2 = 29.9*pi/180
+    # lambda12 = 179.8*pi/180
+    # alpha1 = solve_astroid(a, f, lambda12, phi1, phi2)
+    # print("solution:", 161.914)
+    # print("computed:", alpha1*180/pi)
+
+    # full inverse problem, Table 5
+    phi1 = -30
+    phi2 = 29.9
+    lambda12 = 179.8
+
+    az, backaz, dist = LonLatWGS84.inverse(0.0, phi1, lambda12, phi2)
+    print("solution:", 161.890524, 19989832.827610)
+    print("computed:", az, dist)
